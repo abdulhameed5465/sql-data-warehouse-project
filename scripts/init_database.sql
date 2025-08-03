152dc7653bca0f5938291fa256aeaabd3adf03e9
abdulhameed5465 <153431181+abdulhameed5465@users.noreply.github.com>
2025-08-03 07:36:47 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -10,9 +10,14 @@
 USE master;
 GO
 
--- ================================================
--- Drop existing 'DataWarehouse' database if it exists
--- ================================================
+/* ================================================
+ Drop existing 'DataWarehouse' database if it exists
+    
+    WARNING:
+    Running this script will drop the entire 'DataWarehouse' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+ ================================================ */
 IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
 BEGIN
     ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
